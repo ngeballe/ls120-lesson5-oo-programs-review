4ce322e2169229749c5d4d61aa41656221548b31
Nick Geballe <ngeballe@gmail.com>
2017-03-24 13:45:19 -0400

    Refactor to improve clarity of code for clear screen & display board

--- tic_tac_toe_spike_follow_along.rb
+++ tic_tac_toe_spike_follow_along.rb
@@ -102,8 +102,7 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
-  def display_board(clear = true)
-    system 'clear' if clear
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts
     puts "     |     |"
@@ -118,6 +117,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts    
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -137,7 +141,7 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
     case board.detect_winner
     when human.marker
       puts "You won!"
@@ -160,12 +164,16 @@ class TTTGame
     answer == 'y'
   end
 
-  def play
+  def clear
     system 'clear'
+  end
+
+  def play
+    clear
     display_welcome_message
 
     loop do
-      display_board(false)
+      display_board
       loop do
         human_moves
         break if board.someone_won? || board.full?
@@ -173,12 +181,12 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
       board.reset
-      system 'clear'
+      clear
       puts "Let's play again!"
       puts
     end
