d34a92b809ae800c5a55d83d663c7589787b2502
Nick Geballe <ngeballe@gmail.com>
2017-03-25 16:55:32 -0400

    Create code skeleton for Twenty-One

--- oo_twenty_one.rb
+++ oo_twenty_one.rb
@@ -24,3 +24,77 @@ Game
 - start
 
 '''
+
+class Player
+  def initialize
+    
+  end
+
+  def hit
+    
+  end
+
+  def stay
+    
+  end
+
+  def busted?
+    
+  end
+
+  def total
+    
+  end
+end
+
+class Dealer
+  def initialize
+    # seems like very similar to Player... do we even need this?
+  end
+
+  def deal
+    # does the dealer or the deck deal?
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
+class Participant
+  # what goes in here? all the redundant behaviors from Player and Dealer?
+end
+
+class Deck
+  def initialize
+    
+  end
+
+  def deal
+    
+  end
+end
+
+class Card
+  def initialize
+    
+  end
+end
+
+class Game
+  def start
+    deal_cards
+    show_initial_cards
+    player_turn
+    dealer_turn
+    show_result
+  end
+end
