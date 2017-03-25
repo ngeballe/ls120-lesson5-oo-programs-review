2407284764ca8c50a5c8299efb16b7823469125f
Nick Geballe <ngeballe@gmail.com>
2017-03-25 18:30:37 -0400

    Create my implementation

--- oo_twenty_one.rb
+++ oo_twenty_one.rb
@@ -1,100 +1,216 @@
-# 1. Write a description of the problem and extract major nouns and verbs.
-# 2. Make an initial guess at organizing the verbs into nouns and do a spike to explore the problem with temporary code.
-# 3. Optional - when you have a better idea of the problem, model your thoughts into CRC cards.
-
-# Description:
-# The goal of the game is to get a hand that is closer to a score of 21 than your opponent but not above 21. Number cards are worth their number; Jacks, Queens, and Kings are worth 10, and an Ace can be worth 1 or 11, whichever the player chooses.
-# The dealer deals to cards to him/herself and two to the dealer. The player chooses whether to hit (take another card) or stay (stop). S/he can keep choosing this (and seeing the card dealt) until s/he either busts (goes over 21) or stops. Then the dealer deals cards to him/herself, with the same process. If neither players has busted, after both have stayed, the one whose hand is closer to 21 wins. If a player busts, their opponent wins.
-
-# Nouns: game, player, score, card, dealer -- deck, total
-# Verbs: hit, stay, bust, deal
-
-'''
-Player
-- hit
-- stay
-- busted?
-- total
-- deal ???
-
-Deck
-- deal (here or in Player)
-
-Game
-- start
-
-'''
-
-class Player
-  def initialize
-    
-  end
-
-  def hit
-    
-  end
-
-  def stay
-    
-  end
-
-  def busted?
-    
-  end
-
-  def total
-    
-  end
-end
-
-class Dealer
-  def initialize
-    # seems like very similar to Player... do we even need this?
-  end
-
-  def deal
-    # does the dealer or the deck deal?
-  end
-
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
-  end
-end
+require 'pry'
 
 class Participant
-  # what goes in here? all the redundant behaviors from Player and Dealer?
+  attr_reader :name
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def busted?
+    total > 21
+  end
+
+  def total
+    result = cards.map(&:score).reduce(:+)
+
+    num_aces = cards.count { |card| card.value == 'Ace' }
+    while num_aces > 0 && result > 21
+      result -= 10
+      num_aces -= 1
+    end
+    result
+  end
+
+  def show_cards
+    if cards.size == 2
+      puts "#{name} has #{cards.join(' and ')}."
+    else
+      puts "#{name} has #{cards[0..-2].join(', ')}, and #{cards[-1]}."
+    end
+  end
+
+  def to_s
+    name
+  end
+
+  def show_total
+    result = "#{self} has a total of #{total}."
+    result << " #{self} busted!" if busted?
+    puts result
+  end
+
+  def show_busted_or_stayed
+    if busted?
+      puts "#{self} busted with a total of #{total}."
+    else
+      puts "#{self} stayed with a total of #{total}."
+    end
+  end
+end
+
+class Player < Participant
+  def initialize
+    super
+    @name = set_name
+  end
+
+  def set_name
+    name = nil
+    loop do
+      puts "What's your name?"
+      name = gets.chomp
+      break unless name.empty?
+      puts "Sorry, you must enter a value."
+    end
+    name
+  end
+
+  def show_initial_cards
+    show_cards
+  end
+
+  def hit?
+    answer = nil
+    loop do
+      puts "Do you want to hit or stay?"
+      answer = gets.chomp.downcase
+      break if %w[hit stay].include?(answer)
+      puts "Sorry, that's not a valid choice. You must choose 'hit' or 'stay'."
+    end
+    answer == 'hit'
+  end
+end
+
+class Dealer < Participant
+  def initialize
+    super
+    # seems like very similar to Player... do we even need this?
+    @name = "Dealer"
+  end
+
+  def show_initial_cards
+    puts "#{name} has #{cards[0]} and ?"
+  end
 end
 
 class Deck
+  VALUES = (2..9).to_a + %w[Jack Queen King Ace]
+  SUITS = %w[Hearts Clubs Diamonds Spades]
+
   def initialize
-    
+    @cards = VALUES.product(SUITS).map do |value, suit|
+      Card.new(value, suit)
+    end
+    @cards.shuffle!
   end
 
-  def deal
-    
+  def deal_card(participant)
+    participant.cards << @cards.shift
   end
 end
 
 class Card
-  def initialize
-    
+  attr_reader :value, :suit
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
+  def to_s
+    "the #{value} of #{suit}"
+  end
+
+  def score
+    case value
+    when 2..9
+      value
+    when 'Jack', 'Queen', 'King'
+      10
+    when 'Ace'
+      11
+    end
   end
 end
 
 class Game
+  attr_reader :dealer, :player, :deck
+
+  def initialize
+    @dealer = Dealer.new
+    @player = Player.new
+    @deck = Deck.new
+  end
+
   def start
     deal_cards
     show_initial_cards
     player_turn
-    dealer_turn
+    dealer_turn unless player.busted?
     show_result
   end
+
+  private
+
+  def deal_cards
+    2.times { deck.deal_card(dealer) }
+    2.times { deck.deal_card(player) }
+  end
+
+  def show_initial_cards
+    player.show_initial_cards
+    dealer.show_initial_cards
+  end
+
+  def player_turn
+    loop do
+      break unless player.hit?
+      deck.deal_card(player)
+      player.show_cards
+      break if player.busted?
+    end
+    player.show_busted_or_stayed
+  end
+
+  def dealer_turn
+    while dealer.total < 17
+      deck.deal_card(dealer)
+    end
+    dealer.show_busted_or_stayed
+  end
+
+  def show_result
+    puts
+    player.show_cards
+    player.show_total
+    dealer.show_cards
+    dealer.show_total
+    if winner
+      puts "#{winner} wins!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def winner
+    return winner_if_someone_busted if winner_if_someone_busted
+
+    if dealer.total > player.total
+      dealer
+    elsif player.total > dealer.total
+      player
+    end
+  end
+
+  def winner_if_someone_busted
+    if player.busted?
+      dealer
+    elsif dealer.busted?
+      player
+    end
+  end
 end
+
+Game.new.start
